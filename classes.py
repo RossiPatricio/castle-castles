d2b0687fd8f4c801b813abbc09eb0cf86b8b1328
Patricio Rossi <Patricio Rossi>
2024-11-03 17:19:34 -0300

    Actualizacion 3 3-11-2024

--- classes.py
+++ classes.py
@@ -11,44 +11,49 @@ class Entidad:
     self.estado = "Alive"
 
   def moverse(self, x, y):
-    ...
+    pass
 
   def recibir_daño(self, cantidad):
     self.salud -= cantidad
     if self.salud <= 0:
       self.estado = "Dead"
+      print(f"{self.nombre} ha muerto.")
+
+
 
 class Heroe(Entidad):
-  # Personaje principal
+  # Es controlado por el jugador
   def __init__(self, nombre, salud, daño, fuerza, defensa, energia, velocidad, posicion, tipo):
     super().__init__(nombre, salud, daño, fuerza, defensa, energia, velocidad, posicion)
     self.tipo = tipo
 
-  # Controlado por el jugador
-  def moverse(self, x, y):
-    super().moverse(x, y)
-    # implementar un sistema para que se mueva por eventos de teclado
-    ...
-
   def ataque(self, victima):
     if victima.estado == "Alive":
       victima.recibir_daño(self.daño)
-  
+
+  def ataque_especial(self, victima):
+    if victima.estado == "Alive" and self.energia == 100:
+      print(f"{self.nombre} lanza ataque especial a la victima")
+      victima.recibir_daño(self.daño * 1.5)
+
+
 class Caballero(Heroe):
-  def __init__(self, nombre, salud, ataque, fuerza, defensa, energia, velocidad, posicion):
-    super().__init__(nombre, salud, ataque, fuerza, defensa, energia, velocidad, posicion, tipo= "Caballero")
-
-
-class Ogro(Heroe):
-  def __init__(self, nombre, salud, ataque, fuerza, defensa, energia, velocidad, posicion):
-    super().__init__(nombre, salud, ataque, fuerza, defensa, energia, velocidad, posicion, tipo= "Ogro")
-
-class Mago(Heroe):
-  def __init__(self, nombre, salud, ataque, fuerza, defensa, energia, velocidad, posicion):
-    super().__init__(nombre, salud, ataque, fuerza, defensa, energia, velocidad, posicion, tipo= "Mago")
+  def __init__(self, nombre, salud, daño, fuerza, defensa, energia, velocidad, posicion):
+    super().__init__(nombre, salud, daño, fuerza, defensa, energia, velocidad, posicion, tipo= "Caballero")
 
 
 
 class Enemigo(Entidad):
-  def __init__(self, nombre, salud, ataque, fuerza, defensa, energia, velocidad, posicion):
-    super().__init__(nombre, salud, ataque, fuerza, defensa, energia, velocidad, posicion)
+  # Tiene movimientos preestablecidos
+  def __init__(self, nombre, salud, daño, fuerza, defensa, energia, velocidad, posicion):
+    super().__init__(nombre, salud, daño, fuerza, defensa, energia, velocidad, posicion)
+
+class Murcielago(Enemigo):
+  def __init__(self, salud, posicion):
+    nombre= "Murcielago"
+    daño= 20
+    fuerza= 20
+    defensa= 20
+    energia= 100
+    velocidad= 20
+    super().__init__(nombre, salud, daño, fuerza, defensa, energia, velocidad, posicion)
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from classes import *
 
 
-lancelot = Caballero("Sir Lancelot", 100, 20, 20, 20, 50, 20, (0, 0))
-murcielago = Enemigo("Rick", 20, 5, 5, 5, 5, 10, (0, 0))
+lancelot = Caballero("Sir Lancelot", 100, 20, 20, 20, 100, 20, (0, 0))
+shrek = Ogro("Shrek", 100, 20, 20, 20, 100, 20, (0, 0))
 
-print(murcielago.salud)
-lancelot.ataque(murcielago)
-print(murcielago.salud)
+murcielago1 = Enemigo("Rick", 20, 5, 5, 5, 5, 10, (0, 0))
+
+murcielago2 = Murcielago(20, (0, 0))
+
+lancelot.ataque_especial(murcielago2)
 
